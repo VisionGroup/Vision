e060c88fa167658feb5bb3f0fe94bb50f08a48f2
amir <samirbl@t2.technion.ac.il>
2012-12-12 22:06:04 +0200

    fix speaking clock button

--- MainActivity/src/com/yp2012g4/blindroid/MainActivity.java
+++ MainActivity/src/com/yp2012g4/blindroid/MainActivity.java
@@ -9,115 +9,118 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends onTouchEventClass {
-    /** Called when the activity is first created. */
-    // private TextToSpeech tts;
-    // private Rect rect;
+	/** Called when the activity is first created. */
+	// private TextToSpeech tts;
+	// private Rect rect;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
-	// TODO: Add to every activity!! or to the new Activity intrerface Yaron
-	// is making
-	setVolumeControlStream(AudioManager.STREAM_MUSIC);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// TODO: Add to every activity!! or to the new Activity intrerface Yaron
+		// is making
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-	tts = new TextToSpeech(this, this);
+		tts = new TextToSpeech(this, this);
 
-	// LinearLayout mainView = (LinearLayout)
-	// findViewById(R.id.MainActivityView);
-	// getButtonsPosition(mainView);
-	Button b = (Button) findViewById(R.id.SOSbutton);
-	b.setOnClickListener(new View.OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		speakOut(((Button) v).getText().toString());
-		Intent intent = new Intent(MainActivity.this, SOSActivity.class);
-		startActivity(intent);
-	    }
-	});
-	b.setOnTouchListener(this);
+		// LinearLayout mainView = (LinearLayout)
+		// findViewById(R.id.MainActivityView);
+		// getButtonsPosition(mainView);
+		Button b = (Button) findViewById(R.id.SOSbutton);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				speakOut(((Button) v).getText().toString());
+				Intent intent = new Intent(MainActivity.this, SOSActivity.class);
+				startActivity(intent);
+			}
+		});
+		b.setOnTouchListener(this);
 
-	b = (Button) findViewById(R.id.quickDial);
-	b.setOnClickListener(new View.OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		speakOut(((Button) v).getText().toString());
-		Intent intent = new Intent(MainActivity.this,
-			QuickDialActivity.class);
-		startActivity(intent);
-	    }
-	});
-	b.setOnTouchListener(this);
+		b = (Button) findViewById(R.id.quickDial);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				speakOut(((Button) v).getText().toString());
+				Intent intent = new Intent(MainActivity.this,
+						QuickDialActivity.class);
+				startActivity(intent);
+			}
+		});
+		b.setOnTouchListener(this);
 
-	b = (Button) findViewById(R.id.button3);
-	b.setOnClickListener(new View.OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		speakOut(((Button) v).getText().toString());
-	    }
-	});
-	b.setOnTouchListener(this);
+		b = (Button) findViewById(R.id.button3);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				speakOut(((Button) v).getText().toString());
+			}
+		});
+		b.setOnTouchListener(this);
 
-	b = (Button) findViewById(R.id.button4);
-	b.setOnClickListener(new View.OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		speakOut(((Button) v).getText().toString());
-	    }
-	});
-	b.setOnTouchListener(this);
+		b = (Button) findViewById(R.id.button4);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				speakOut(((Button) v).getText().toString());
+			}
+		});
+		b.setOnTouchListener(this);
 
-	b = (Button) findViewById(R.id.button5);
-	b.setOnClickListener(new View.OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		speakOut(((Button) v).getText().toString());
-	    }
-	});
-	b.setOnTouchListener(this);
+		b = (Button) findViewById(R.id.button5);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				speakOut(((Button) v).getText().toString());
+			}
+		});
+		b.setOnTouchListener(this);
 
-	b = (Button) findViewById(R.id.button6);
-	b.setOnClickListener(new View.OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		speakOut(((Button) v).getText().toString());
-	    }
-	});
-	b.setOnTouchListener(this);
+		b = (Button) findViewById(R.id.button6);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				speakOut(((Button) v).getText().toString());
+			}
+		});
+		b.setOnTouchListener(this);
 
-	b = (Button) findViewById(R.id.clock);
-	b.setOnClickListener(new View.OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		speakOut(((Button) v).getText().toString());
-	    }
-	});
-	b.setOnTouchListener(this);
+		b = (Button) findViewById(R.id.clock);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				speakOut(((Button) v).getText().toString());
+				Intent intent = new Intent(MainActivity.this,
+						SpeakingClockActivity.class);
+				startActivity(intent);
+			}
+		});
+		b.setOnTouchListener(this);
 
-	b = (Button) findViewById(R.id.button8);
-	b.setOnClickListener(new View.OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		speakOut(((Button) v).getText().toString());
-		Intent intent = new Intent(MainActivity.this,
-			PhoneStatusActivity.class);
-		startActivity(intent);
-	    }
-	});
-	b.setOnTouchListener(this);
-    }
+		b = (Button) findViewById(R.id.button8);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				speakOut(((Button) v).getText().toString());
+				Intent intent = new Intent(MainActivity.this,
+						PhoneStatusActivity.class);
+				startActivity(intent);
+			}
+		});
+		b.setOnTouchListener(this);
+	}
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-	super.onWindowFocusChanged(hasFocus);
-	ViewGroup mainView = (ViewGroup) findViewById(R.id.MainActivityView);
-	// Resources res = getResources();
-	// XmlResourceParser parser = res.getXml(R.layout.activity_main);
-	// getButtonsPosition(mainView, parser);
-	// InputStream inputStream =
-	// getResources().openRawResource(R.layout.activity_main);
-	// File file = new File(inputStream);
-	getButtonsPosition(mainView);
-    }
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		ViewGroup mainView = (ViewGroup) findViewById(R.id.MainActivityView);
+		// Resources res = getResources();
+		// XmlResourceParser parser = res.getXml(R.layout.activity_main);
+		// getButtonsPosition(mainView, parser);
+		// InputStream inputStream =
+		// getResources().openRawResource(R.layout.activity_main);
+		// File file = new File(inputStream);
+		getButtonsPosition(mainView);
+	}
 
 }

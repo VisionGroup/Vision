49427aace0f8527e07b076807a47b8bd7647f84f
maytal <mitshm@gmail.com>
2012-12-12 16:57:29 +0200

    Initial

--- MainActivity/gen/com/yp2012g4/blindroid/R.java
+++ MainActivity/gen/com/yp2012g4/blindroid/R.java
@@ -17,7 +17,6 @@ public final class R {
     public static final class id {
         public static final int SOS_textview=0x7f07000a;
         public static final int SOSbutton=0x7f070000;
-        public static final int analogClock1=0x7f07000b;
         public static final int button3=0x7f070002;
         public static final int button4=0x7f070003;
         public static final int button5=0x7f070004;
@@ -26,16 +25,13 @@ public final class R {
         public static final int button_getBatteryStatus=0x7f070009;
         public static final int button_getReceptionStatus=0x7f070008;
         public static final int clock=0x7f070006;
-        public static final int menu_settings=0x7f07000e;
+        public static final int menu_settings=0x7f07000b;
         public static final int quickDial=0x7f070001;
-        public static final int textView1=0x7f07000c;
-        public static final int textView2=0x7f07000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_phone_status=0x7f030001;
         public static final int activity_sos=0x7f030002;
-        public static final int activity_speaking_clock=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;

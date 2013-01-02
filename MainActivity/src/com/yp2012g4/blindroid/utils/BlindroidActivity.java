27f74d09c3b9ceb7dca46f72664e5991d6832964
amir blumental <samirbl@t2.technion.ac.il>
2013-01-02 22:43:00 +0200

    removed the title bar and status bar from all the activities that inherit from BlindroidActivity.java

--- MainActivity/src/com/yp2012g4/blindroid/utils/BlindroidActivity.java
+++ MainActivity/src/com/yp2012g4/blindroid/utils/BlindroidActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.view.Window;
+import android.view.WindowManager;
 
 public abstract class BlindroidActivity extends onTouchEventClass {
   private int _icon;
@@ -22,19 +24,25 @@ public abstract class BlindroidActivity extends onTouchEventClass {
     return _toolTip;
   }
   
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setVolumeControlStream(AudioManager.STREAM_MUSIC);
-  }
-  
-  @Override public void onDestroy() {
-    super.onDestroy();
-  }
-  
   public void init(Activity activity, int icon, String name, String toolTip) {
     _t = new TTS(activity, (OnInitListener) activity);
     _icon = icon;
     _name = name;
     _toolTip = toolTip;
+  }
+  
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // hide titlebar of application
+    // must be before setting the layout
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    // hide statusbar of Android
+    // could also be done later
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    setVolumeControlStream(AudioManager.STREAM_MUSIC);
+  }
+  
+  @Override public void onDestroy() {
+    super.onDestroy();
   }
 }

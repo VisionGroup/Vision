01d5d82928d59d593812c53ddc8567da62b8ea0e
Amir Blumental <samirbl@t2.technion.ac.il>
2013-01-05 16:25:39 +0200

    added to phone status - unread SMS read and missed calls read

--- MainActivity/src/com/yp2012g4/blindroid/PhoneStatusActivity.java
+++ MainActivity/src/com/yp2012g4/blindroid/PhoneStatusActivity.java
@@ -1,12 +1,17 @@
 package com.yp2012g4.blindroid;
 
+import java.util.ArrayList;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -33,7 +38,8 @@ public class PhoneStatusActivity extends BlindroidActivity {
     protected SignalStrengthListener() {
     }
     
-    @Override public void onSignalStrengthsChanged(android.telephony.SignalStrength signalStrength) {
+    @Override
+    public void onSignalStrengthsChanged(android.telephony.SignalStrength signalStrength) {
       // get the signal strength (a value between 0 and 31)
       _signal = signalStrength.getGsmSignalStrength();
       super.onSignalStrengthsChanged(signalStrength);
@@ -48,6 +54,7 @@ public class PhoneStatusActivity extends BlindroidActivity {
   int _status = -1; // Charging Status
   int _signal = -1; // Reception status
   SignalStrengthListener signalStrengthListener;
+  final ArrayList<String> contacts = new ArrayList<String>();
   
   /**
    * Returns the chargeStatus String according to charge status param. Empty
@@ -66,7 +73,36 @@ public class PhoneStatusActivity extends BlindroidActivity {
     }
   }
   
-  @Override public int getViewId() {
+  public String getMissedCalls() {
+    String[] projection = { CallLog.Calls.CACHED_NAME, CallLog.Calls.NUMBER, CallLog.Calls.TYPE };
+    String where = CallLog.Calls.TYPE + "=" + CallLog.Calls.MISSED_TYPE + " AND NEW = 1";
+    Cursor c = managedQuery(CallLog.Calls.CONTENT_URI, projection, where, null, null);
+    String s = "";
+    boolean b = true;
+    while (c.moveToNext()) {
+      String name = c.getString(1);
+      if (!b)
+        s += " And from ";
+      else
+        b = false;
+      s += name;
+    }
+    if (c.getCount() == 0)
+      return "There are no missed calls";
+    return "You have " + c.getCount() + " missed calls from " + s;
+  }
+  
+  public String getUnreadSMS() {
+    final Uri SMS_INBOX = Uri.parse("content://sms/inbox");
+    Cursor c = managedQuery(SMS_INBOX, null, "read = 0", null, null);
+    int unreadMessagesCount = c.getCount();
+    if (unreadMessagesCount == 0)
+      return "There are no unread SMS messages";
+    return "You have " + unreadMessagesCount + " unread SMS";
+  }
+  
+  @Override
+  public int getViewId() {
     return R.id.phoneStatusActivity;
   }
   
@@ -75,7 +111,8 @@ public class PhoneStatusActivity extends BlindroidActivity {
    * 
    * @see android.view.View.OnClickListener#onClick(android.view.View)
    */
-  @Override public void onClick(View v) {
+  @Override
+  public void onClick(View v) {
     Resources res = getResources();
     switch (v.getId()) {
       case R.id.button_getBatteryStatus:
@@ -84,6 +121,12 @@ public class PhoneStatusActivity extends BlindroidActivity {
         break;
       case R.id.button_getReceptionStatus:
         speakOut(signalToString(_signal));
+        break;
+      case R.id.button_getMissedCalls:
+        speakOut(getMissedCalls());
+        break;
+      case R.id.button_getUnreadSMS:
+        speakOut(getUnreadSMS());
         break;
       case R.id.back_button:
         speakOut("Previous screen");
@@ -109,15 +152,16 @@ public class PhoneStatusActivity extends BlindroidActivity {
   /**
    * onCreate method.
    */
-  @Override protected void onCreate(Bundle savedInstanceState) {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     init(this, 0/* TODO Check what icon goes here */, getString(R.string.phoneStatus_whereami),
         getString(R.string.phoneStatus_help));
     setContentView(R.layout.activity_phone_status);
-
     // Battery Broadcast receiver.
     BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
-      @Override public void onReceive(Context context, Intent intent) {
+      @Override
+      public void onReceive(Context context, Intent intent) {
         _battery = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
         _status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
       }
@@ -127,14 +171,14 @@ public class PhoneStatusActivity extends BlindroidActivity {
     // start the signal strength listener
     signalStrengthListener = new SignalStrengthListener();
     ((TelephonyManager) getSystemService(TELEPHONY_SERVICE)).listen(signalStrengthListener,
-        PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);  
+        PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
   }
   
-  @Override public void onWindowFocusChanged(boolean hasFocus) {
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
     super.onWindowFocusChanged(hasFocus);
-    if (hasFocus) {
+    if (hasFocus)
       speakOut("Phone status screen");
-    }
   }
   
   /**

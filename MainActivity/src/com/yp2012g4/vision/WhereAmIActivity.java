e16dfd68d45548b052eb840ba4f6df20516fa24c
syauster <syauster@t2.technion.ac.il>
2013-01-25 12:28:07 +0200

    remove onCreateOptionsMenu from all activity's

--- MainActivity/src/com/yp2012g4/vision/WhereAmIActivity.java
+++ MainActivity/src/com/yp2012g4/vision/WhereAmIActivity.java
@@ -116,9 +116,9 @@ public class WhereAmIActivity extends VisionActivity {
     return false;
   }
   
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.activity_main, menu);
-    return true;
-  }
+//  @Override public boolean onCreateOptionsMenu(Menu menu) {
+//    // Inflate the menu; this adds items to the action bar if it is present.
+//    getMenuInflater().inflate(R.menu.activity_main, menu);
+//    return true;
+//  }
 }

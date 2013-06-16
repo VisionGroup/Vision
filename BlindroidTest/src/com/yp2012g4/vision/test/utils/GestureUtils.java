abf56361eca9baf71f9d64769dd93dcf991cc368
amirb <amirbl100@gmail.com>
2013-06-16 18:09:29 +0300

    try to fix favorite contacts

--- BlindroidTest/src/com/yp2012g4/vision/test/utils/GestureUtils.java
+++ BlindroidTest/src/com/yp2012g4/vision/test/utils/GestureUtils.java
@@ -4,6 +4,10 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.view.Display;
 
+import com.jayway.android.robotium.solo.Solo;
+import com.yp2012g4.vision.R;
+import com.yp2012g4.vision.apps.smsReader.DeleteConfirmation;
+
 public class GestureUtils {
   public static void flingRight(final ActivityInstrumentationTestCase2<?> c) {
     fling(c, -150);
@@ -21,5 +25,17 @@ public class GestureUtils {
     final int screenHeight = getDefaultDisplay(c).getHeight();
     final int screenWidth = getDefaultDisplay(c).getWidth();
     TouchUtils.drag(c, screenWidth / 2, screenWidth / 2 + offset, screenHeight / 2, screenHeight / 2, 20);
+  }
+  
+  /**
+   * @param confirmDelete
+   *          test
+   */
+  public static void useDeleteConfirmation(final boolean confirmDelete, final Solo solo, final ActivityInstrumentationTestCase2<?> c) {
+    solo.assertCurrentActivity("wrong activity", DeleteConfirmation.class);
+    if (confirmDelete)
+      GestureUtils.flingRight(c);
+    else
+      solo.clickOnView(solo.getView(R.id.Delete_Confirmation_Button));
   }
 }
